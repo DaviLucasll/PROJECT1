4fd7f04431482ab6cb7e19d8a2a6878eb9cdfa57
Davi Lucas <124807556+DaviLucasll@users.noreply.github.com>
2023-02-23 13:52:02 -0300

    test

--- PROJETO1/index.js
+++ PROJETO1/index.js
@@ -4,26 +4,32 @@ const skyCourseBtn = document.querySelector("#sky-btn")
 lightCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
+    <div class="course-slide-txt">
         <h1>Comece a programar do zero gratuitamente</h1>
         <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
         <button>QUERO CONHECER</button>
-        `
+    </div>  
+    `
 })
 
 cloudCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
-    <h1>Comece a programar do zero gratuitamente</h1>
-    <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
-    <button>QUERO CONHECER</button>
+    <div class="course-slide-txt">
+        <h1>Comece a programar do zero gratuitamente</h1>
+        <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
+        <button>QUERO CONHECER</button>
+    </div>
     `
 })
 
 skyCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
-    <h1>Comece a programar do zero gratuitamente</h1>
-    <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
-    <button class="course-slide-btn">QUERO CONHECER</button>
+    <div class="course-slide-txt">
+        <h1>Comece a programar do zero gratuitamente</h1>
+        <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
+        <button>QUERO CONHECER</button>
+    </div>
     `
 })
