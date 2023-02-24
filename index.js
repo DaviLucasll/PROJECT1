3c84e10a2ef052a16135606d7d349db8067b5279
Davi Lucas <daviluccas0705@gmail.com>
2023-02-24 11:24:33 -0300

    slide courses update

--- index.js
+++ index.js
@@ -4,32 +4,41 @@ const skyCourseBtn = document.querySelector("#sky-btn")
 lightCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
-    <div class="course-slide-txt">
-        <h1>Comece a programar do zero gratuitamente</h1>
-        <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
-        <button>QUERO CONHECER</button>
-    </div>  
+    <div class="course-slide-left">
+        <h1 class="course-slide-headline">Comece a programar do zero gratuitamente</h1>
+        <p class="course-slide-description">
+            Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.
+        </p>
+        <button class="course-slide-btn">QUERO CONHECER</button>
+    </div>
+    <img src="" alt=""> 
     `
 })
 
 cloudCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
-    <div class="course-slide-txt">
-        <h1>Comece a programar do zero gratuitamente</h1>
-        <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
-        <button>QUERO CONHECER</button>
+    <div class="course-slide-left">
+        <h1 class="course-slide-headline">Comece a programar do zero gratuitamente</h1>
+        <p class="course-slide-description">
+            Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.
+        </p>
+        <button class="course-slide-btn">QUERO CONHECER</button>
     </div>
+    <img src="" alt="">
     `
 })
 
 skyCourseBtn.addEventListener("click",()=>{
     const lightCourseSlide = document.querySelector("#course-slide")
     lightCourseSlide.innerHTML = `
-    <div class="course-slide-txt">
-        <h1>Comece a programar do zero gratuitamente</h1>
-        <p>Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.</p>
-        <button>QUERO CONHECER</button>
+    <div class="course-slide-left">
+        <h1 class="course-slide-headline">Comece a programar do zero gratuitamente</h1>
+        <p class="course-slide-description">
+            Você vai encontrar aulas para dominar HTML, CSS, JavaScript, HTTP, Forms, Ferramentas, DOM e muito mais.
+        </p>
+        <button class="course-slide-btn">QUERO CONHECER</button>
     </div>
+    <img src="" alt="">
     `
 })
