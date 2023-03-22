e7b41be7050cbac3c7e6569f4c88d19f6b240f09
Davi Lucas <daviluccas0705@gmail.com>
2023-03-22 14:24:36 -0300

    mobile header update

--- options.js
+++ options.js
@@ -1,7 +1,10 @@
-const mobileMenu = document.querySelector(".mobile-menu")
-mobileMenu.addEventListener("click", ()=>{
-    mobileMenu.classList.toggle("open")
-    const mobileMenuOptions = document.querySelector(".mobile-menu-list")
-    console.log(mobileMenuOptions)
-    mobileMenuOptions.classList.toggle("display-flex")
-})
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuList = document.querySelector(".mobile-menu-list");
+const mobileListItems = document.quer;
+mobileMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+  mobileMenuList.classList.toggle("open");
+  const mobileMenuOptions = document.querySelector(".mobile-menu-list");
+  console.log(mobileMenuOptions);
+  mobileMenuOptions.classList.toggle("display-flex");
+});
